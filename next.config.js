b4a9e66a1ca804eb4e2ec180c00868ce85aed7e6
Oein <oein0219@gmail.com>
2023-01-06 18:39:33 +0900

    add route

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  async rewrites() {
+    return [
+      {
+        source: "/:path*",
+        destination: `${process.env.API}/s/:path*?sec=${process.env.SECRET}`,
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
