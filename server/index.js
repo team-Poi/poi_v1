9079a5bce3d51af71909fb37cc0a6be4bef057c6
Oein <oein0219@gmail.com>
2023-01-06 18:39:21 +0900

    backend done!

--- server/index.js
+++ server/index.js
@@ -1,12 +1,21 @@
 import express from "express";
 import { PrismaClient } from "@prisma/client";
+import * as dotenv from "dotenv";
+
 const prisma = new PrismaClient();
 const app = express();
+
+app.use((req, res, next) => {
+  let sec = req.query.sec;
+  if (decodeURI(sec).trim() != decodeURI(process.env.SECRET.trim()))
+    res.status(404).send(`Cannot ${req.method} ${req.url}`);
+  else next();
+});
 
 app.get("/s/:id", (req, res) => {
   let id = req.params.id;
   prisma.shortURL
-    .findUnique({
+    .findFirst({
       where: {
         short: id,
       },
@@ -14,9 +23,13 @@ app.get("/s/:id", (req, res) => {
     .then((v) => {
       if (v) res.status(301).redirect(v.long);
       else res.status(301).redirect("https://poi.kr/404");
+    })
+    .catch((e) => {
+      res.status(500).send(e.message);
     });
 });
 
+dotenv.config();
 app.listen(8210, () => {
   console.log("Server listening on port 8210");
 });
