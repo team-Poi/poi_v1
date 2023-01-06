14295733d962637423e194847c2b14b6a5490394
Oein <oein0219@gmail.com>
2023-01-06 12:14:57 +0900

    add server

--- server/index.js
+++ server/index.js
@@ -1,11 +1,20 @@
 import express from "express";
-
+import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient();
 const app = express();
 
 app.get("/s/:id", (req, res) => {
   let id = req.params.id;
-  let url = "https://google.com"; // TODO : Get from db
-  res.status(301).redirect("https://google.com");
+  prisma.shortURL
+    .findUnique({
+      where: {
+        short: id,
+      },
+    })
+    .then((v) => {
+      if (v) res.status(301).redirect(v.long);
+      else res.status(301).redirect("https://poi.kr/404");
+    });
 });
 
 app.listen(8210, () => {
